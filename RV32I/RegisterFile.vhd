312be8eea041fed9d03fe1ea3b7403b7402d7520
Gioele <87376700+GioeleSe@users.noreply.github.com>
2025-08-25 11:44:36 +0200

    control signals changed, bit gate for reading
    Separate control signals to split completely read and write operations.
    Reading operations are still fully combinatorial.

--- RV32I/RegisterFile.vhd
+++ RV32I/RegisterFile.vhd
@@ -6,17 +6,17 @@ use work.common.ALL;
 entity RegisterFile is
     port(
         clock: in std_logic;
-        mem_enable: in std_logic;
-        mem_operation: in MEM_OP_T;
-
+        
         addr_A: in std_logic_vector(4 downto 0);
         addr_B: in std_logic_vector(4 downto 0);
+        read_enable: in std_logic;
         
         addr_C: in std_logic_vector(4 downto 0);                    -- write operations use a dedicated port
         write_C: in std_logic_vector(31 downto 0);
+        write_enable: in std_logic;
 
         val_A: out std_logic_vector(31 downto 0);
-        val_B: out std_logic_vector(31 downto 0);
+        val_B: out std_logic_vector(31 downto 0)
     );
 end entity;
 
@@ -25,17 +25,18 @@ architecture behaviour of RegisterFile is
     signal reg_memory: REG_MEMORY_T := (others => (others => '0')); -- start with all registers to 0
     
     begin
-        process(clock)
+        process(clock)                                              -- only write operations are synchronous
         begin
             if rising_edge(clock) then
-                if mem_enable = '1' and mem_operation = OP_STORE then
+                if write_enable = '1' then
                     if addr_C /= "00000" then                       -- don't write on x0
                         reg_memory(addr_C) <= write_C;
                     end if;
                 end if;
             end if;
         end process;
-
-        val_A <= (others => '0') when addr_A = "00000" else reg_memory(to_integer(unsigned(addr_A)));
-        val_B <= (others => '0') when addr_B = "00000" else reg_memory(to_integer(unsigned(addr_B)));
+        if read_enable = '1' then                                   -- bit gate to prevent unwanted memory operations
+            val_A <= (others => '0') when addr_A = "00000" else reg_memory(to_integer(unsigned(addr_A)));
+            val_B <= (others => '0') when addr_B = "00000" else reg_memory(to_integer(unsigned(addr_B)));
+        end if;
 end architecture;
